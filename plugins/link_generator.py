df434b73afccb6e87385409ab376e658c9716945
vpcreatz <119391455+vpcreatz@users.noreply.github.com>
2023-05-06 16:48:32 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -36,8 +36,8 @@ async def batch(client: Client, message: Message):
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://omegalinks.in/api?api=c3b055af24586d0d06c0e750a56d591024f7c7be&url=https://telegram.me/share/url?url={link}&alias=RESULT')]])
-    await second_message.reply_text(f"<b>Here is your link</b>\n\nhttps://omegalinks.in/api?api=c3b055af24586d0d06c0e750a56d591024f7c7be&url={link}&alias=RESULT", quote=True, reply_markup=reply_markup)
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://omegalinks.in/st?api=c3b055af24586d0d06c0e750a56d591024f7c7be&url=https://telegram.me/share/url?url={link}')]])
+    await second_message.reply_text(f"<b>Here is your link</b>\n\nhttps://omegalinks.in/st?api=c3b055af24586d0d06c0e750a56d591024f7c7be&url={link}", quote=True, reply_markup=reply_markup)
 
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
@@ -56,5 +56,5 @@ async def link_generator(client: Client, message: Message):
 
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://omegalinks.in/api?api=c3b055af24586d0d06c0e750a56d591024f7c7be&url=https://telegram.me/share/url?url={link}&alias=RESULT')]])
-    await channel_message.reply_text(f"<b>Here is your link</b>\n\nhttps://omegalinks.in/api?api=c3b055af24586d0d06c0e750a56d591024f7c7be&url=https://telegram.me/share/url?url={link}&alias=RESULT", quote=True, reply_markup=reply_markup)
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://omegalinks.in/st?api=c3b055af24586d0d06c0e750a56d591024f7c7be&url={link}')]])
+    await channel_message.reply_text(f"<b>Here is your link</b>\n\nhttps://omegalinks.in/st?api=c3b055af24586d0d06c0e750a56d591024f7c7be&url={link}", quote=True, reply_markup=reply_markup)
